75a988486f57f28aa91e0727da2f3635caefa726
QuanTrieuPCYT <admin@qtpc.tech>
2025-12-05 09:43:19 +0700

    implement primitive evo80 support, with open sourced library and VIAL insecure mode

---
+++ keyboards/evoworks/evo80/keymaps/vial/config.h
@@ -0,0 +1,23 @@
+/* Copyright 2025 Carlos Eduardo de Paula <carlosedp@gmail.com>
+ * Copyright 2025 EPOMAKER <https://github.com/Epomaker>
+ * Copyright 2023 LiWenLiu <https://github.com/LiuLiuQMK>
+ * Copyright 2021 QMK <https://github.com/qmk/qmk_firmware>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// Custom configuration
+
+#define VIAL_KEYBOARD_UID {0xD5, 0x0A, 0xF6, 0xF9, 0x74, 0x79, 0x16, 0xE3}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
